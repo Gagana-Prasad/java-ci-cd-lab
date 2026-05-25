309239137b6076c0dd26316f62941761720433e1
Gagana <gaganaprasad2020@gmail.com>
2026-05-25 02:03:37 -0700

    fix calculator errors

--- calculator.java
+++ calculator.java
@@ -1,20 +1,18 @@
-public class calculator{
-    public int add(int a,int b){
-        return a+b;
-         public int subtraction(int a, int b) {
+public class calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b) {
         return a - b;
     }
+
     public int multiplication(int a, int b) {
         return a * b;
     }
+
     public int division(int a, int b) {
         return a / b;
-    }
-     if (calc.division(20, 4) == 5) {
-            System.out.println("Division Test Passed");
-        } else {
-            System.out.println("Division Test Failed");
-        }
-
     }
 }
--- calculatorTest.java
+++ calculatorTest.java
@@ -1,23 +1,28 @@
-public class calculatorTest{
-    public static void main(String[] args){
-        calculator cal=new calculator();
-        if(cal.add(2,3)==5){
-            System.out.println("Test Passed");
-        
-        }else{
-            System.out.println("Test Failed")
+public class calculatorTest {
+
+    public static void main(String[] args) {
+
+        calculator calc = new calculator();
+
+        if (calc.add(5, 3) == 8) {
+            System.out.println("Addition Test Passed");
+        } else {
+            System.out.println("Addition Test Failed");
         }
-         if (calc.subtraction(10, 4) == 6) {
+
+        if (calc.subtraction(10, 4) == 6) {
             System.out.println("Subtraction Test Passed");
         } else {
             System.out.println("Subtraction Test Failed");
         }
+
         if (calc.multiplication(2, 5) == 10) {
             System.out.println("Multiplication Test Passed");
         } else {
             System.out.println("Multiplication Test Failed");
         }
-         if (calc.division(20, 4) == 5) {
+
+        if (calc.division(20, 4) == 5) {
             System.out.println("Division Test Passed");
         } else {
             System.out.println("Division Test Failed");
